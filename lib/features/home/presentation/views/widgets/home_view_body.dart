26d08c8482b303558d2209a4401fb906e8603382
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2023-09-25 17:17:00 +0300

    create app styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -8,19 +8,50 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(24),
+    return Padding(
+      padding: const EdgeInsets.all(24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomAppBar(),
-          FeaturedbooksListView(),
-          SizedBox(
+          const CustomAppBar(),
+          const FeaturedbooksListView(),
+          const SizedBox(
             height: 50,
           ),
-          Text(
+          const Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 130,
+            child: Row(
+              children: [
+                AspectRatio(
+                  aspectRatio: 1.3 / 2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://img.freepik.com/free-psd/halloween-celebration-poster-template_23-2150733079.jpg?w=740&t=st=1695639929~exp=1695640529~hmac=eb01b527d3c693a1cd3f7ae6522d6d3c4e17d256a95be12c0ab69cc215db5886'),
+                            fit: BoxFit.fill)),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                const Column(
+                  children: [
+                    Text(
+                      'Harry Potter',
+                      //'Harry Potter and the Goblet of Fire',
+                      style: Styles.textStyle20,
+                      maxLines: 2,
+                    )
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
