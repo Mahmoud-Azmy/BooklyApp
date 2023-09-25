9488aad4a1fa835116f90c18cd30251bbb33a44f
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2023-09-25 10:20:10 +0300

    create CustomAppBar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          AssetsData.klogo,
+          height: 20,
+        ),
+        const Spacer(),
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass))
+      ],
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
