3e842d00a775f37b91935e73c4f2c8bd11b5263c
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2023-09-30 13:32:55 +0300

    create NewsetBooksCubit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -1,6 +1,12 @@
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class FeaturedBooksState {}
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
 
@@ -9,11 +15,11 @@ class FeaturedBooksLoading extends FeaturedBooksState {}
 class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
 
-  FeaturedBooksSuccess(this.books);
+  const FeaturedBooksSuccess(this.books);
 }
 
 class FeaturedBooksFailure extends FeaturedBooksState {
   final String errorMessage;
 
-  FeaturedBooksFailure(this.errorMessage);
+  const FeaturedBooksFailure(this.errorMessage);
 }
---
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  static NewsetBooksCubit get(context) => BlocProvider.of(context);
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
+}
