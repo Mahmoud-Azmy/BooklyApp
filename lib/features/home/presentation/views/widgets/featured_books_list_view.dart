b82a0728e85877a693fd39e47a5e6bcae8416a0c
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2023-10-01 17:17:47 +0300

    add BlocBuilder to FeaturedBooksListView

--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,5 +1,10 @@
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/core/widgets/err_message.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedbooksListView extends StatelessWidget {
   const FeaturedbooksListView({
@@ -8,18 +13,28 @@ class FeaturedbooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.28,
-      child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const CustomBookImage();
-          },
-          separatorBuilder: (context, index) => const SizedBox(
-                width: 16,
-              ),
-          itemCount: 20),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.28,
+            child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const CustomBookImage();
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                      width: 16,
+                    ),
+                itemCount: 20),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return ErrMessage(errMessage: state.errorMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
